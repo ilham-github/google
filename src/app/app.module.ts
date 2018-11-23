85ccda6d7ef36a25ca40f179fcafcc3fb45189ce
ilham <ilhama6573@gmail.com>
2018-11-23 20:17:14 +0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { GooglePlus } from '@ionic-native/google-plus';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import firebase from 'firebase';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+export const config = {
+  apiKey: "AIzaSyAgj2G7MBrXVds1H5Q0h962z740ctsVR1w",
+    authDomain: "plus-e0969.firebaseapp.com",
+    databaseURL: "https://plus-e0969.firebaseio.com",
+    projectId: "plus-e0969",
+    storageBucket: "plus-e0969.appspot.com",
+    messagingSenderId: "653069597755"
 
+}
+firebase.initializeApp(config)
 @NgModule({
   declarations: [
     MyApp,
@@ -34,6 +43,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    GooglePlus,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { GooglePlus } from '@ionic-native/google-plus';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private googlePlus: GooglePlus) {
+   
   }
-
+  login(){
+  this.googlePlus.login({})
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+}
 }
